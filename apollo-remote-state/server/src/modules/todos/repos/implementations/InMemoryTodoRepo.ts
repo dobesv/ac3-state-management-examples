5b52ea37f6c93468e7012f1442b7f411202ffb86
Dobes Vandermeer <dobes@formative.com>
2024-05-23 13:27:09 -0700

    Add prettier

--- apollo-remote-state/server/src/modules/todos/repos/implementations/InMemoryTodoRepo.ts
+++ apollo-remote-state/server/src/modules/todos/repos/implementations/InMemoryTodoRepo.ts
@@ -10,57 +10,63 @@ export class InMemoryTodoRepo implements TodoRepo {
 
   private lastTodoId: number = this.todos.length;
 
-  constructor () {
+  constructor() {}
 
-  }
-
-  public async addTodo (text: string): Promise<void> {
-    if (text.length < 3) throw new Error("Todo needs to be longer than 3 characters.")
+  public async addTodo(text: string): Promise<void> {
+    if (text.length < 3)
+      throw new Error("Todo needs to be longer than 3 characters.");
     this.lastTodoId++;
-    this.todos.push({ id: this.lastTodoId, text, completed: false })
-    console.log('New todo list', this.todos)
+    this.todos.push({ id: this.lastTodoId, text, completed: false });
+    console.log("addTodo ->", this.todos);
   }
 
-  public async completeTodo (id: number): Promise<void> {
-    this.todos = this.todos.map((t) => t.id === id ? { ...t, completed: true } : t)
+  public async completeTodo(id: number): Promise<void> {
+    this.todos = this.todos.map((t) =>
+      t.id === id ? { ...t, completed: true } : t,
+    );
+    console.log("completeTodo ->", this.todos);
   }
 
-  public async clearCompletedTodos (): Promise<void> {
-    this.todos = this.todos
-      .filter((t) => t.completed !== true);
+  public async clearCompletedTodos(): Promise<void> {
+    this.todos = this.todos.filter((t) => t.completed !== true);
+    console.log("clearCompletedTodos ->", this.todos);
   }
 
   public async deleteTodo(id: number): Promise<void> {
-    this.todos = this.todos.filter((todo: Todo) => todo.id !== id)
+    this.todos = this.todos.filter((todo: Todo) => todo.id !== id);
+    console.log("deleteTodo ->", this.todos);
   }
 
   public async editTodo(id: number, text: string): Promise<void> {
-    if (text.length < 3) throw new Error("Todo needs to be longer than 3 characters.")
+    if (text.length < 3)
+      throw new Error("Todo needs to be longer than 3 characters.");
     const found = this.todos.findIndex((t) => t.id === id);
     if (found === -1) {
-      throw new Error("Todo not found for editing")
+      throw new Error("Todo not found for editing");
     }
 
     this.todos[found].text = text;
+    console.log("editTodo ->", this.todos);
   }
 
-  public async getAllTodos (): Promise<Todo[]> {
+  public async getAllTodos(): Promise<Todo[]> {
     return this.todos;
   }
 
-  public async getTodoById (id: number): Promise<Todo> {
+  public async getTodoById(id: number): Promise<Todo> {
     const found = this.todos.findIndex((t) => t.id === id);
     if (found === -1) {
-      throw new Error("Todo not found")
+      throw new Error("Todo not found");
     }
     return this.todos[found];
   }
 
-  public async getLastTodo (): Promise<Todo> {
-    return this.todos[this.todos.length - 1]
+  public async getLastTodo(): Promise<Todo> {
+    return this.todos[this.todos.length - 1];
   }
 
-  public async completeAllTodos (): Promise<void> {
-    this.todos = this.todos.map((t) => ({ ...t, completed: true }))
+  public async completeAllTodos(): Promise<void> {
+    this.todos = this.todos.map((t) => ({ ...t, completed: true }));
+    console.log("completeAllTodos ->", this.todos);
   }
 }
